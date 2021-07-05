8eb3967d7cd74670a40e9e4966ee6d5e5623e58c
SmithWebDev <smithwebdd@gmail.com>
2021-07-05 00:25:02 -0400

    Cleaned up city spec file refactor

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,33 +1,5 @@
 require 'rails_helper'
 
-# describe City do
-#   describe 'validations' do
-#     it 'is valid with a name and population number' do
-#       city = build(:city)
-#       expect(city).to be_valid
-#     end
-#     it 'is invalid without a name' do
-#       city = build(:city, name: nil)
-#       city.valid?
-#       expect(city.errors[:name]).to include "can't be blank"
-#     end
-#     it 'is invalid without a population' do
-#       city = build(:city, population: nil)
-#       city.valid?
-#       expect(city.errors[:population]).to include "can't be blank"
-#     end
-#   end
-#   describe 'associations' do
-#     it 'has belongs to a country' do
-#       assc = described_class.reflect_on_association(:country)
-#       expect(assc.macro).to eq :belongs_to
-#     end
-#   end
-#   it 'has a valid factory' do
-#     expect(build(:city)).to be_valid
-#   end
-# end
-
 describe City do
   context 'associations' do
     it { should belong_to(:country) }
