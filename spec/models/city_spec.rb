7c4140ea3069f5557a5f4b4b73a67874fb7e9e26
SmithWebDev <smithwebdd@gmail.com>
2021-07-05 00:23:34 -0400

    Added shoulda matcher refactor of City spec file

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,27 +1,40 @@
 require 'rails_helper'
 
+# describe City do
+#   describe 'validations' do
+#     it 'is valid with a name and population number' do
+#       city = build(:city)
+#       expect(city).to be_valid
+#     end
+#     it 'is invalid without a name' do
+#       city = build(:city, name: nil)
+#       city.valid?
+#       expect(city.errors[:name]).to include "can't be blank"
+#     end
+#     it 'is invalid without a population' do
+#       city = build(:city, population: nil)
+#       city.valid?
+#       expect(city.errors[:population]).to include "can't be blank"
+#     end
+#   end
+#   describe 'associations' do
+#     it 'has belongs to a country' do
+#       assc = described_class.reflect_on_association(:country)
+#       expect(assc.macro).to eq :belongs_to
+#     end
+#   end
+#   it 'has a valid factory' do
+#     expect(build(:city)).to be_valid
+#   end
+# end
+
 describe City do
-  describe 'validations' do
-    it 'is valid with a name and population number' do
-      city = build(:city)
-      expect(city).to be_valid
-    end
-    it 'is invalid without a name' do
-      city = build(:city, name: nil)
-      city.valid?
-      expect(city.errors[:name]).to include "can't be blank"
-    end
-    it 'is invalid without a population' do
-      city = build(:city, population: nil)
-      city.valid?
-      expect(city.errors[:population]).to include "can't be blank"
-    end
+  context 'associations' do
+    it { should belong_to(:country) }
   end
-  describe 'associations' do
-    it 'has belongs to a country' do
-      assc = described_class.reflect_on_association(:country)
-      expect(assc.macro).to eq :belongs_to
-    end
+  context 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :population }
   end
   it 'has a valid factory' do
     expect(build(:city)).to be_valid
