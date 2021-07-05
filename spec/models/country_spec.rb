195ec9729f291e1dc39433cdc7ea6a5f029748a9
SmithWebDev <smithwebdd@gmail.com>
2021-07-04 20:08:19 -0400

    updated country_spec file, maintained original test to show progressions

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -18,3 +18,32 @@ describe Country do
     expect(country.errors[:population]).to include("can't be blank")
   end
 end
+
+describe Country do
+  describe 'validations' do
+    it 'is valid with a name and a population number' do
+      country = build(:country)
+      expect(country).to be_valid
+    end
+
+    it 'is invalid without a name' do
+      country = build(:country, name: nil)
+      country.valid?
+      expect(country.errors[:name]).to include "can't be blank"
+    end
+    it 'is invalid without a population' do
+      country = build(:country, population: nil)
+      country.valid?
+      expect(country.errors[:population]).to include "can't be blank"
+    end
+  end
+  describe 'associations' do
+    it 'has many cities' do
+      assc = described_class.reflect_on_association(:cities)
+      expect(assc.macro).to eq :has_many
+    end
+  end
+  it 'has a valid factory' do
+    expect(build(:country)).to be_valid
+  end
+end
