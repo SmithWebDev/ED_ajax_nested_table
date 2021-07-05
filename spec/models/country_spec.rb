cbcdbf5dcfb03bbef3c38e41d5e182e80a6613e3
SmithWebDev <smithwebdd@gmail.com>
2021-07-04 20:09:07 -0400

    Cleaned country_spec file to contain a single test suite

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,25 +1,6 @@
 require 'rails_helper'
 
 describe Country do
-  it 'is valid with a name and a population number' do
-    country = Country.new
-    country.name = 'France'
-    country.population = 67_892_374_217_863_796
-    expect(country).to be_valid
-  end
-  it 'is invalid without a name' do
-    country = Country.new(name: nil)
-    country.valid?
-    expect(country.errors[:name]).to include("can't be blank")
-  end
-  it 'is invalid without a population number' do
-    country = Country.new(population: nil)
-    country.valid?
-    expect(country.errors[:population]).to include("can't be blank")
-  end
-end
-
-describe Country do
   describe 'validations' do
     it 'is valid with a name and a population number' do
       country = build(:country)
