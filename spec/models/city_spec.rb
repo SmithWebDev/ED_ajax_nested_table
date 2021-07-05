e6fb388f3e82ddbe0a7e3602c4b2513b37c8f25a
SmithWebDev <smithwebdd@gmail.com>
2021-07-04 20:52:12 -0400

    Updated City spec with factory syntax

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -19,3 +19,31 @@ describe City do
     expect(city.errors[:population]).to(include("can't be blank"))
   end
 end
+
+describe City do
+  describe 'validations' do
+    it 'is valid with a name and population number' do
+      city = build(:city)
+      expect(city).to be_valid
+    end
+    it 'is invalid without a name' do
+      city = build(:city, name: nil)
+      city.valid?
+      expect(city.errors[:name]).to include "can't be blank"
+    end
+    it 'is invalid without a population' do
+      city = build(:city, population: nil)
+      city.valid?
+      expect(city.errors[:population]).to include "can't be blank"
+    end
+  end
+  describe 'associations' do
+    it 'has belongs to a country' do
+      assc = described_class.reflect_on_association(:country)
+      expect(assc.macro).to eq :belongs_to
+    end
+  end
+  it 'has a valid factory' do
+    expect(build(:city)).to be_valid
+  end
+end
