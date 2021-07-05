4e0cdff31cb9ac40efeb796749d59894702f1925
dB <smithwebdd@gmail.com>
2021-07-05 15:09:38 -0400

    initial "completion" commit, mistakes where made, bugs will be found and killed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :cities
   resources :countries
+  get 'countries/cities/:id', to: 'countries#cities', as: 'countries_cities'
   root to: 'countries#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- spec/features/user_creates_city_spec.rb
+++ spec/features/user_creates_city_spec.rb
@@ -2,21 +2,22 @@ require 'rails_helper'
 
 feature 'Cities management' do
   let!(:country) do
-    create(:country, name: 'Colombia', population: 500000000000)
+    create(:country, name: 'Colombia', population: 500_000_000_000)
   end
 
-  scenario "User creates city" do
-    visit root_path
-    expect{
+  scenario 'User creates city' do
+    visit '/'
+
+    expect do
       click_link 'New City'
       select 'Colombia', from: 'Country'
       fill_in 'Name', with: 'Bogota'
       fill_in 'Population', with: '5000000000'
       click_button 'Create City'
-    }.to change(City, :count).by(1)
+    end.to change(City, :count).by(1)
     city = City.last
     expect(city).not_to be_nil
     expect(current_path).to eq city_path(city)
-    expect(page).to have_content "City was successfully created."
+    expect(page).to have_content 'City was successfully created.'
   end
 end
