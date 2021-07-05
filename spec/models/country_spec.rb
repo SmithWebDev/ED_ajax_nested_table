9fff89574b2955e0ef05b89d549b2b092e31a7e8
SmithWebDev <smithwebdd@gmail.com>
2021-07-05 00:17:44 -0400

    refactored test spec with shoulda matcher, maintained previous refactor

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -28,3 +28,16 @@ describe Country do
     expect(build(:country)).to be_valid
   end
 end
+
+describe Country do
+  context 'associations' do
+    it { should have_many :cities }
+  end
+  context 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :population }
+  end
+  it 'has a valid factory' do
+    expect(build(:country)).to be_valid
+  end
+end
